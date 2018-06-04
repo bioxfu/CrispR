e256a8c00d7f56078298d5f23a6facb05eb04d5a
Fu Xing <bio.xfu@gmail.com>
2018-06-04 10:24:08 +0800

    update

--- script/CripsRVariants.R
+++ script/CripsRVariants.R
@@ -4,13 +4,13 @@ library(GenomicFeatures)
 library(gridExtra)
 txdb <- loadDb('./txdb/tair10_txdb.sqlite')
 
-plates <- c('R1-R6_T2-1-1', 
-           'R1-R6_T2-1-2',
+plates <- c(#'R1-R6_T2-1-1', 
+           #'R1-R6_T2-1-2',
            'R7-12_T1-1-1', 
            'R7-12_T1-1-2')
            
-gd_fnames <- c('guide/R1-R6.bed',
-              'guide/R1-R6.bed',
+gd_fnames <- c(#'guide/R1-R6.bed',
+              #'guide/R1-R6.bed',
               'guide/R19-R24.bed',
               'guide/R19-R24.bed')
               
@@ -36,11 +36,13 @@ for (N in 1:length(plates)) {
         reference <- Biostrings::reverseComplement(Biostrings::DNAString(reference))
       }
       crispr_set <- readsToTarget(bam_fnames, target = gdl[i], reference = reference, names = sample_names, target.loc = 22, chimeras='ignore', verbose=FALSE)
-      p <- plotVariants(crispr_set, txdb=txdb, gene.text.size=8,
-                        row.ht.ratio=c(1,8), col.wdth.ratio=c(4,2),
-                        plotAlignments.args = list(line.weight=0.5, ins.size=2, legend.symbol.size=4),
-                        plotFreqHeatmap.args = list(plot.text.size=3, x.size=8, legend.text.size=8,
-                                                    legend.key.height=grid::unit(0.5, "lines")))
+      if (!is.null(crispr_set)) {
+        p <- plotVariants(crispr_set, txdb=txdb, gene.text.size=8,
+                          row.ht.ratio=c(1,8), col.wdth.ratio=c(4,2),
+                          plotAlignments.args = list(line.weight=0.5, ins.size=2, legend.symbol.size=4),
+                          plotFreqHeatmap.args = list(plot.text.size=3, x.size=8, legend.text.size=8,
+                                                      legend.key.height=grid::unit(0.5, "lines")))
+      }
       dev.off()
     }
   }
