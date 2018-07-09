1a52032c703178a25302c8fa8b3788c5f473a71f
Fu Xing <bio.xfu@gmail.com>
2018-07-09 14:12:12 +0800

    update

--- script/CripsRVariants.R
+++ script/CripsRVariants.R
@@ -9,12 +9,12 @@ plates <- c('R1-R6_T2-1-1',
            'R7-12_T1-1-1', 
            'R7-12_T1-1-2')
            
-gd_fnames <- c('guide/R1-R6.bed',
-              'guide/R1-R6.bed',
-              'guide/R19-R24.bed',
-              'guide/R19-R24.bed')
+gd_fnames <- c('guide/gRNA_R1-R6.bed',
+              'guide/gRNA_R1-R6.bed',
+              'guide/gRNA_R19-R24.bed',
+              'guide/gRNA_R19-R24.bed')
 
-var_table <- NULL
+indel_table <- NULL
 
 for (N in 1:length(plates)) {
   plate <- plates[N]
@@ -32,29 +32,29 @@ for (N in 1:length(plates)) {
       
       bam_fnames <- dir(paste0('bam/', plate), pattern = paste0(RP, '_[0-9]+.bam$'), full.names = TRUE)
       sample_names <- sub('.bam', '', dir(paste0('bam/', plate), pattern = paste0(RP, '_[0-9]+.bam$')))
+      bam_fnames <- bam_fnames[c(1,5:12,2:4)]
+      sample_names <- sample_names[c(1,5:12,2:4)]
       reference <- system(sprintf('samtools faidx index/tair10.fa %s:%s-%s', seqnames(gdl)[i], start(gdl)[i], end(gdl)[i]), intern = TRUE)[[2]]
-      if (as.character(strand(gdl)[i]) == '-') {
-        reference <- Biostrings::reverseComplement(Biostrings::DNAString(reference))
-      }
       crispr_set <- readsToTarget(bam_fnames, target = gdl[i], reference = reference, names = sample_names, target.loc = 22, chimeras='ignore', verbose=FALSE)
       if (!is.null(crispr_set) && sum(grepl('no variant', rownames(crispr_set$cigar_freqs))) == 1) {
-        # pdf(paste0(folder, '/', plate, '_', gname, '_', RP, '.pdf'), wid=15, hei=15)
-        # p <- plotVariants(crispr_set, txdb=txdb, gene.text.size=8,
-        #                   row.ht.ratio=c(1,8), col.wdth.ratio=c(4,2),
-        #                   plotAlignments.args = list(line.weight=0.5, ins.size=2, legend.symbol.size=4),
-        #                   plotFreqHeatmap.args = list(plot.text.size=3, x.size=8, legend.text.size=8,
-        #                                               legend.key.height=grid::unit(0.5, "lines")))
-        # dev.off()
+        pdf(paste0(folder, '/', plate, '_', gname, '_', RP, '.pdf'), wid=15, hei=15)
+        p <- plotVariants(crispr_set, txdb=txdb, gene.text.size=8,
+                          row.ht.ratio=c(1,8), col.wdth.ratio=c(4,2),
+                          plotAlignments.args = list(line.weight=0.5, ins.size=2, legend.symbol.size=4),
+                          plotFreqHeatmap.args = list(plot.text.size=3, x.size=8, legend.text.size=8,
+                                                      legend.key.height=grid::unit(0.5, "lines")))
+        dev.off()
 
         freqs <- crispr_set$cigar_freqs
         freqs_all <- colSums(freqs)
-        freqs_var <- freqs_all - freqs['no variant',]
-        var_percent <- round(freqs_var / freqs_all * 100, 2)
+        freqs_indel <- colSums(freqs[-c(1, grep('SNV', rownames(freqs))),])
+        indel_percent <- round(freqs_indel / freqs_all * 100, 2)
         sample_names <- names(freqs_all)
-        var_table <- rbind(var_table, as.data.frame(cbind(plate, gname, sample_names, freqs_all, freqs_var, var_percent)))
+        indel_table <- rbind(indel_table, as.data.frame(cbind(plate, gname, sample_names, freqs_all, freqs_indel, indel_percent)))
       }
     }
   }
 }
 
-write.table(var_table, './tables/variant_freq_all.tsv', row.names = F, quote=F, sep='\t')
+write.table(indel_table, './tables/indel_freq_all.tsv', row.names = F, quote=F, sep='\t')
+
--- workflow.sh
+++ workflow.sh
@@ -10,6 +10,7 @@ script/split_fastq_by_barcode.py -f raw/JKZ0259/R1-R6_T2-1-2/JKZ0259_5672_R1.fq.
 ## Mapping
 module add bwa/0.7.15
 module add samtools/1.3
+
 # build index
 cp /home/xfu/Gmatic5/genome/tair10/tair10.fa index/tair10.fa
 bwa index index/tair10.fa
@@ -18,5 +19,11 @@ ls split/ |./script/rush -k 'mkdir -p bam/{}'
 find split/*|sed -n 's/_R[12].fastq.gz//p'|sort|uniq|./script/rush -k 'bwa mem index/tair10.fa {}_R1.fastq.gz {}_R2.fastq.gz | samtools view -Shb | samtools sort -o bam/{/%}/{%@split/(.+?)/}.bam'
 ls bam/*/*|parallel --gnu 'samtools index {}'
 
+# gRNA position
+module add bowtie/1.1.2
+bowtie -f ~/Gmatic5/genome/tair10/tair10 guide/gRNA_R1-R6.fa |awk '{print $3"\t"$4"\t"$4+length($5)"\t"$1"\t0\t"$2}' > guide/gRNA_R1-R6.bed
+bowtie -f ~/Gmatic5/genome/tair10/tair10 guide/gRNA_R19-R24.fa |awk '{print $3"\t"$4"\t"$4+length($5)"\t"$1"\t0\t"$2}' > guide/gRNA_R19-R24.bed
+
 ## CripsRVariants
 ~/R/3.5.0/bin/Rscript script/CripsRVariants.R
+
